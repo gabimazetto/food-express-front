1ad7d116813a3c0dc9a668966c5e01baa2366e80
Diego Arruda <arrudadiego1@gmail.com>
2023-05-09 13:15:33 -0300

    Inicio da página de Cadastro

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Root } from "./pages/Root/Root";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
+import { NovoCliente } from "./pages/NovoCliente/NovoCliente";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Root />}>
-          <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<Login />}/>
-        </Route>        
-      </Routes>    
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/novocliente" element={<NovoCliente />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
