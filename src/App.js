0d8dc51d0213827438f9fedff21f88855afd359f
ZamboniA <andersonzamboni18@gmail.com>
2023-05-10 14:22:49 -0300

    adicionar comida

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Root } from "./pages/Root/Root";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import { NovoCliente } from "./pages/NovoCliente/NovoCliente";
+import { NovoRestaurante } from "./pages/NovoRestaurante/NovoRestaurante";
 import { AdicionarComida } from "./pages/AdicionarComida/AdicionarComida";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/novocliente" element={<NovoCliente />} />
+          <Route path="/novorestaurante" element={<NovoRestaurante />} />
           <Route path="/restaurante/cardapio/nova" element={<AdicionarComida/>}/>
         </Route>        
       </Routes>    
