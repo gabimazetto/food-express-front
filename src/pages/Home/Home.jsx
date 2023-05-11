8e62f9e573a31d147925eb143cfcca5dfc382314
vanessaneves-dev <vanessaneves.dev@gmail.com>
2023-05-11 09:50:15 -0300

    salvando localmente

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
+import svgtest from "../../assets/icons/Design sem nome258.svg"
+
 
 export function Home(){
     return (
         <div className="home">
             <h1>HOME</h1>
+            <img src={svgtest} alt="gffd" />
         </div>
     )
 }
