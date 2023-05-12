d3b941c6425d7d2a2b7f0c70b912a083d4dd1d59
ZamboniA <andersonzamboni18@gmail.com>
2023-05-12 11:46:01 -0300

    Implementação da página HOME de restaurantes, falta autenticação.

--- src/pages/HomeRestaurante/HomeRestaurante.jsx
+++ src/pages/HomeRestaurante/HomeRestaurante.jsx
@@ -13,8 +13,8 @@ export function HomeRestaurante() {
                         <div className="titulo-home-restaurante">
                             <h1>Cardápio</h1>
                             <div className="button-header invisivel-mobile ">
-                                <Button className="button-meus-itens" variant="warning">Meus itens</Button>
-                                <Button className="button-add-itens" variant="warning">Adicionar novo item</Button>
+                                <Button className="button-meus-itens" variant="primary">Meus itens</Button>
+                                <Button className="button-add-itens" variant="primary">Adicionar novo item</Button>
                             </div>
                         </div>
                         <div className="imagem-home-restaurante">
@@ -22,7 +22,7 @@ export function HomeRestaurante() {
                         </div>
                     </div>
                     <div className="invisivel-desktop button-home-restaurante">
-                        <Button className="button-header" variant="warning">Adicionar refeição</Button>
+                        <Button className="button-header" variant="primary">Adicionar refeição</Button>
                     </div>
                 </header>
                 <CardCardapioRestaurante />
