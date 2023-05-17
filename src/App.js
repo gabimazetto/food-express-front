7ab5f2a918f917d92025811964e0915d2ad7fbd9
TamiresDellaRovere <tamires.rovere@hotmail.com>
2023-05-17 11:13:28 -0300

    Alteração pagina de cadastrar cliente;

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Root } from "./pages/Root/Root";
 import { Home } from "./pages/Home/Home";
 import { CadastroCliente } from "./pages/CadastroCliente/CadastroCliente";
-import { CadastroClienteParte1 } from "./pages/CadastroClienteParte1/CadastoClienteParte1";
 import { Cardapio } from "./pages/Cardapio/Cardapio";
 import { EditaRestaurante } from "./pages/EditarRestaurante/EditarRestaurante";
 import { EditarCliente } from "./pages/EditarCliente/EditarCliente";
@@ -14,7 +13,6 @@ import { HomeCliente } from "./pages/HomeCliente/HomeCliente";
 import { CadastroRestaurante } from "./pages/CadastroRestaurante/CadastroRestaurante";
 import { LoginRestaurante } from "./pages/LoginRestaurante/LoginRestaurante";
 import { Restaurantes } from "./pages/Restaurantes/Restaurantes";
-import { CardCardapioCliente } from "./components/CardCardapioCliente/CardCardapioCliente";
 import { PedidosCliente } from "./pages/PedidosCliente/PedidosCliente";
 import { Favoritos } from "./pages/Favoritos/Favoritos";
 
@@ -26,28 +24,26 @@ function App() {
       <Routes>
         <Route path="/" element={<Root />}>
           <Route path="/" element={<Home />} />
-        dev
+          dev
           <Route path="/cliente/login" element={<LoginCliente />} />
-          <Route path="/restaurante/login" element={<LoginRestaurante />} />          
-        main
+          <Route path="/restaurante/login" element={<LoginRestaurante />} />
+          main
 
           <Route path="/cliente/home" element={<HomeCliente />} />
           <Route path="/cliente/cadastro" element={<CadastroCliente />} />
           <Route path="/cliente/perfil/:id" element={<EditarCliente />} />
           <Route path="/cliente/listar/restaurantes" element={<Restaurantes />} />
           <Route path="/cliente/listar/favoritos" element={<Favoritos />} />
-          <Route path="/cliente/pedidos" element={<PedidosCliente/>} />
+          <Route path="/cliente/pedidos" element={<PedidosCliente />} />
           <Route path="/restaurante/cadastro" element={<CadastroRestaurante />} />
-          <Route path="/restaurante/home" element={<HomeRestaurante/>}/>
+          <Route path="/restaurante/home" element={<HomeRestaurante />} />
           <Route path="/restaurante/id/cardapio" element={<Cardapio />} />  {/* Alterar a rota também no adicionar/atualizar comida*/}
           <Route path="/restaurante/cardapio/cadastro" element={<AdicionarAtualizarComida />} />         {/* Adicionar nova comida ao cardapio*/}
-          <Route path="/restaurante/cardapio/item/:id" element={<AdicionarAtualizarComida/>}/>       {/* Atualizar comida no cardapio*/}          
+          <Route path="/restaurante/cardapio/item/:id" element={<AdicionarAtualizarComida />} />       {/* Atualizar comida no cardapio*/}
           <Route path="/restaurante/:id" element={<PerfilRestaurante />} />
           <Route path="/restaurante/perfil/:id" element={<EditaRestaurante />} />
-
         </Route>
-        <Route path="/cadastrocliente" element={<CadastroClienteParte1 />} />         {/* manter essa ou a da linha 30*/}
-      
+
       </Routes>
     </BrowserRouter>
   );
--- src/pages/CadastroCliente/CadastroCliente.jsx
+++ src/pages/CadastroCliente/CadastroCliente.jsx
@@ -1,272 +1,220 @@
 import axios from "axios";
-import { Button, Card, Col, Form, Row } from "react-bootstrap";
+import { useState } from "react";
+import {
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { CustomInput } from "../../components/CustomInput/CustomInput";
+import { CustomInputIconNone } from "../../components/CustomInputIconNone/CustomInputIconNone";
 
 export function CadastroCliente() {
+  const [senha, setSenha] = useState("password");
+  const [icone, setIcone] = useState("bi bi-eye-slash text-light");
+  function mudarTipo() {
+    if (senha === "password") {
+      setIcone("bi bi-eye-fill text-light");
+      setSenha("text");
+    } else {
+      setIcone("bi bi-eye-slash text-light");
+      setSenha("password");
+    }
+  }
+
+  const [senha2, setSenha2] = useState("password");
+  const [icone2, setIcone2] = useState("bi bi-eye-slash text-light");
+  function mudarTipo2() {
+    if (senha2 === "password") {
+      setIcone2("bi bi-eye-fill text-light");
+      setSenha2("text");
+    } else {
+      setIcone2("bi bi-eye-slash text-light");
+      setSenha2("password");
+    }
+  }
+
   const {
     register,
     handleSubmit,
     formState: { errors },
+    getValues,
   } = useForm();
   const navigate = useNavigate();
+  const dayjs = require("dayjs");
+  const today = dayjs();
 
   function onSubmit(data) {
+    console.log(data);
     axios
       .post("http://localhost:3001/clientes", data)
       .then((response) => {
+        toast.success("Cliente adicionado", {
+          position: "bottom-right",
+          duration: 2000,
+        });
         navigate("/cliente/login");
       })
       .catch((error) => {
+        toast.error("Algo deu errado", {
+          position: "bottom-right",
+          duration: 2000,
+        });
         console.log(error);
       });
   }
 
   return (
-    <div className="container mt-2 ">
-      <Card className="mt-3 mb-3" border="warning">
-        <Card.Body>
-          <h1 className="text-center">Cadastro</h1>
+    <>
+      <div className="container mt-2 ">
+        <Card className="mt-3 mb-3" border="warning">
+          <Card.Body>
+            <h1 className="text-center">Cadastro Cliente</h1>
 
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group className="mt-2 mb-3 ">
-              <Form.Label>Nome Completo</Form.Label>
-              <Form.Control
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <CustomInputIconNone
+                className="input-web"
                 type="text"
-                placeholder="Arnaldo Correia"
-                className={errors.nome && "is-invalid"}
-                {...register("nome", {
-                  required: "O nome completo é obrigatório.",
+                placeholder="Informe seu nome"
+                icon=""
+                register={register("nome", { required: "O nome é obrigatório" })}
+                error={errors.nome}
+              />
+
+              <CustomInput
+                className="input-web"
+                type="email"
+                placeholder="Informe seu e-mail"
+                icon="bi bi-envelope-at-fill white "
+                register={register("email", { required: "O email é obrigatório" })}
+                error={errors.email}
+              />
+
+              <CustomInput
+                className="input-web"
+                type={senha}
+                placeholder="Crie sua senha"
+                icon={icone}
+                register={register("senha", {
+                  required: "A senha é obrigatória",
+                })}
+                error={errors.senha}
+                toggleType={mudarTipo}
+                iconType={icone}
+              />
+
+              <CustomInput
+                className="input-web"
+                type={senha2}
+                placeholder="Confirme sua senha"
+                icon={icone2}
+                register={register("confirmarSenha", {
+                  required: "As senhas devem ser iguais",
+                  validate: (value) =>
+                    value === getValues("senha") ||
+                    "As senhas devem ser iguais",
+                })}
+                error={errors.confirmarSenha}
+                toggleType={mudarTipo2}
+                iconType={icone2}
+              />
+
+              <CustomInput
+                label="Telefone"
+                type="tel"
+                icon="bi bi-telephone white"
+                className="input-web"
+                pattern="[(0-9)]{4}[0-9]{5}-[0-9]{4}|[0-9]{2}[0-9]{5}-[0-9]"
+                placeholder="Informe seu telefone no formato: (99)99999-9999 ou 1212345-1234"
+                register={register("telefone", {
+                  required: "O telefone é obrigatório.",
                   maxLength: {
-                    value: 130,
-                    message: "Limite de 130 caracteres.",
+                    value: 14,
+                    message: "Limite de 14 caracteres.",
                   },
                 })}
+                error={errors.telefone}
               />
-              {errors.nome && (
-                <Form.Text className="invalid-feedback">
-                  {errors.nome.message}
-                </Form.Text>
-              )}
-            </Form.Group>
 
-            <Form.Group className="mb-3">
-              <Form.Label>Telefone</Form.Label>
-              <Form.Control
-                type="tel"
-                className={errors.telefone && "is-invalid"}
-                placeholder="(99)99999-9999"
-                pattern="[(0-9)]{4}[0-9]{5}-[0-9]{4}|[0-9]{2}[0-9]{5}-[0-9]"
-                title="(99)99999-9999 ou 1212345-1234"
-                {...register("telefone", {
-                  required: "O telefone é obrigatório.",
-                  maxLength: { value: 14, message: "Limite de 14 caracteres." },
-                })}
-              />
-              {errors.telefone && (
-                <Form.Text className="invalid-feedback">
-                  {errors.telefone.message}
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>CPF</Form.Label>
-              <Form.Control
-                type="text"
-                className={errors.cpf && "is-invalid"}
-                placeholder="999.999.999-99"
-                pattern="[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}|[0-9]{9}-[0-9]{2}"
-                title="Formato: 123.456.789-12 ou 123456789-12"
-                {...register("cpf", {
-                  required: "O CPF é obrigatório.",
-                  maxLength: { value: 14, message: "Limite de 14 caracteres." },
-                })}
-              />
-              {errors.cpf && (
-                <Form.Text className="invalid-feedback">
-                  {errors.cpf.message}
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Data de Nascimento</Form.Label>
-              <Form.Control
+              <CustomInput
                 type="date"
-                className={errors.dataNascimento && "is-invalid"}
-                {...register("dataNascimento")}
+                icon="bi bi-calendar-date white"
+                min={dayjs("1920-01-01").format("YYYY-MM-DD")}
+                max={today.subtract(13, "year").format("YYYY-MM-DD")}
+                className="input-web"
+                register={register("dataNascimento")}
               />
-            </Form.Group>
-            <Row>
-              <Col>
-                <Form.Group className="mb-3">
-                  <Form.Label>Cidade</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Alabasta"
-                    className={errors.endereco?.cidade && "is-invalid"}
-                    {...register("endereco.cidade", {
-                      required: "A cidade é obrigatória.",
-                      maxLength: {
-                        value: 255,
-                        message: "Limite de 255 caracteres.",
-                      },
-                    })}
-                  />
-                  {errors.endereco?.cidade && (
-                    <Form.Text className="invalid-feedback">
-                      {errors.endereco?.cidade.message}
-                    </Form.Text>
-                  )}
-                </Form.Group>
-              </Col>
-              {/* Dropdown aqui */}
-              {/* <Form.Group as={Col}>
-                <Dropdown className="mt-4" drop="end">
-                  <Dropdown.Toggle variant="warning">UF</Dropdown.Toggle>
-                  <Dropdown.Menu className="super-colors">
-                    <Dropdown.Item>SP</Dropdown.Item>
-                    <Dropdown.Item>CE</Dropdown.Item>
-                    <Dropdown.Item>CE</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">CE</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">CE</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">CE</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">CE</Dropdown.Item>
-                    <Dropdown.Item
-                      href="#/action-3"
-                      {...register("endereco.uf", {
-                        required: "O UF é obrigatório.",
-                        maxLength: {
-                          value: 2,
-                          message: "Limite de 2 caracteres",
-                        },
-                      })}
-                    >
-                      PE
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                  {errors.endereco?.uf && (
-                    <Form.Text className="invalid-feedback">
-                      {errors.endereco?.uf.message}
-                    </Form.Text>
-                  )}
-                </Dropdown>
-              </Form.Group> */}
-              <Form.Group as={Col} className="mb-3">
-                <Form.Label>UF</Form.Label>
-                <Form.Select
-                  aria-label="Default select example"
-                  className={errors.endereco?.uf && "is-invalid"}
-                  {...register("endereco.uf", {
-                    required: "O UF é obrigatório.",
-                    maxLength: { value: 2, message: "Limite de 2 caracteres" },
-                  })}
-                >
-                  <option value="default">Unidade Federativa</option>
-                  <option value="1">PE</option>
-                  <option value="2">SP</option>
-                  <option value="3">RJ</option>
-                  <option value="4">CE</option>
-                </Form.Select>
-                {errors.endereco?.uf && (
-                  <Form.Text className="invalid-feedback">
-                    {errors.endereco?.uf.message}
-                  </Form.Text>
-                )}
-              </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label>CEP</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="99999-999"
-                  pattern="[0-9]{5}-[0-9]{3}|[0-9]{8}"
-                  title="Formato: 123.456.789-12"
-                  className={errors.endereco?.cep && "is-invalid"}
-                  {...register("endereco.cep", {
-                    required: "O CEP é obrigatório.",
-                    maxLength: { value: 9, message: "Limite de 9 caracteres." },
-                  })}
-                />
-                {errors.endereco?.cep && (
-                  <Form.Text className="invalid-feedback">
-                    {errors.endereco?.cep.message}
-                  </Form.Text>
-                )}
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col} className="mb-3">
-                <Form.Label>Rua</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Avenida Paulista"
-                  className={errors.endereco?.rua && "is-invalid"}
-                  {...register("endereco.rua", {
-                    required: "A rua é obrigatória.",
-                    maxLength: {
-                      value: 100,
-                      message: "Limite de 100 caracteres.",
-                    },
-                  })}
-                />
-                {errors.endereco?.rua && (
-                  <Form.Text className="invalid-feedback">
-                    {errors.endereco?.rua.message}
-                  </Form.Text>
-                )}
-              </Form.Group>
+              <CustomInputIconNone
+                className="input-web"
+                type="text"
+                pattern="[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}|[0-9]{9}-[0-9]{2}"
+                placeholder="Informe seu CPF no formato: 123.456.789-12 ou 123456789-12"
+                icon=""
+                register={register("cpf", { required: "O CPF é obrigatório." })}
+                error={errors.cpf}
+              />
 
-              <Form.Group as={Col} className="mb-3">
-                <Form.Label>Numero</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="5"
-                  className={errors.endereco?.numero && "is-invalid"}
-                  {...register("endereco.numero", {
-                    required: "O número é obrigatório.",
-                    maxLength: {
-                      value: 100,
-                      message: "Limite de 100 caracteres.",
-                    },
-                  })}
-                />
-                {errors.endereco?.numero && (
-                  <Form.Text className="invalid-feedback">
-                    {errors.endereco?.numero.message}
-                  </Form.Text>
-                )}
-              </Form.Group>
+              <CustomInputIconNone
+                className="input-web"
+                type="text"
+                placeholder="Informe sua cidade"
+                register={register("endereco.cidade", { required: "A Cidade é obrigatória" })}
+                error={errors.endereco?.cidade}
+              />
 
-              <Form.Group as={Col} className="mb-3">
-                <Form.Label>Complemento</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Casa ou Apt"
-                  className={errors.endereco?.complemento && "is-invalid"}
-                  {...register("endereco.complemento", {
-                    maxLength: {
-                      value: 100,
-                      message: "Limite de 100 caracteres.",
-                    },
-                  })}
-                />
-                {errors.endereco?.complemento && (
-                  <Form.Text className="invalid-feedback">
-                    {errors.endereco?.complemento.message}
-                  </Form.Text>
-                )}
-              </Form.Group>
-            </Row>
-            <div className="d-flex justify-content-center">
-              <Button variant="warning" type="submit" color="light">
-                Cadastrar
-              </Button>
-            </div>
-          </Form>
-        </Card.Body>
-      </Card>
-    </div>
+              <CustomInputIconNone
+                className="input-web"
+                type="text"
+                placeholder="Informe seu estado com dois caracteres ex: SP"
+                register={register("endereco.uf", { required: "O Estado é obrigatório" })}
+                error={errors.endereco?.uf}
+              />
+
+              <CustomInputIconNone
+                className="input-web"
+                type="text"
+                placeholder="Informe seu CEP no formato: 99999-999"
+                pattern="[0-9]{5}-[0-9]{3}|[0-9]{8}"
+                register={register("endereco.cep", { required: "O CEP é obrigatório" })}
+                error={errors.endereco?.cep}
+              />
+
+              <CustomInputIconNone
+                className="input-web"
+                type="text"
+                placeholder="Informe seu logradouro"
+                register={register("endereco.rua", { required: "A rua é obrigatória." })}
+                error={errors.endereco?.rua}
+              />
+
+              <CustomInputIconNone
+                className="input-web"
+                type="text"
+                placeholder="Informe seu número residencial"
+                register={register("endereco.numero", { required: "O número é obrigatório." })}
+                error={errors.endereco?.numero}
+              />
+
+              <CustomInputIconNone
+                className="input-web"
+                type="text"
+                placeholder="Informe um complemento"
+                register={register("endereco.complemento")}
+                error={errors.endereco?.complemento}
+              />
+
+              <div className="d-flex justify-content-center">
+                <Button variant="warning" type="submit" color="light">
+                  Cadastrar
+                </Button>
+              </div>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
   );
 }
