767a14f0e3c51926040dc8abaea1c7444c742006
ZamboniA <andersonzamboni18@gmail.com>
2023-05-19 19:49:19 -0300

    Correção da descrição e listagem de restaurante

--- src/pages/DescricaoPedidoCliente/DescricaoPedidoCliente.jsx
+++ src/pages/DescricaoPedidoCliente/DescricaoPedidoCliente.jsx
@@ -10,8 +10,8 @@ import { ContextClient } from "../../contexts/ClientContext";
 
 
 export function DescricaoPedidoCliente() {
-    const [descricaoPedidos, setdescricaoPedidos] = useState([]);
-    const { idCli} = useContext(ContextClient);
+    const [descricaoPedidos, setdescricaoPedidos] = useState(null);
+    const { idCli } = useContext(ContextClient);
     const { id } = useParams();
     const frete = 7.50;
     const cupom = 4.50;
@@ -21,15 +21,40 @@ export function DescricaoPedidoCliente() {
     }, []);
 
     function initializeTable() {
-        axios.get(`http://localhost:3001/pedidos/cliente/${idCli}/${id}`)
+        axios
+            .get(`http://localhost:3001/pedidos/cliente/${idCli}/${id}`)
             .then((response) => {
-                setdescricaoPedidos(response.data);
-                console.log(response.data);
+                const { cliente, enderecoPedido, items, restaurante, ...pedido } = response.data;
+
+                const listaPedidos = {
+                    ...pedido,
+                    cliente: { nome: cliente.nome, cpf: cliente.cpf },
+                    enderecoPedido: { ...enderecoPedido },
+                    items: items.map(({ quantidade, comida }) => ({
+                        quantidade,
+                        comida: {
+                            nome: comida.nome,
+                            preco: comida.preco
+                        },
+                    })),
+                    restaurante: { nomeFantasia: restaurante.nomeFantasia },
+                };
+
+                setdescricaoPedidos(listaPedidos);
             })
             .catch((error) => {
                 toast.error("Erro ao carregar dados.");
             });
     }
+
+    const calcularValorTotalItens = () => {
+        if (descricaoPedidos.items) {
+            return descricaoPedidos.items
+                .map(item => item.quantidade * parseFloat(item.comida?.preco))
+                .reduce((a, b) => a + b, 0);
+        }
+        return 0;
+    };
 
     return (
         <div className="container-desc-cliente">
@@ -49,17 +74,18 @@ export function DescricaoPedidoCliente() {
                         <div className="vertical-row-desc"></div>
                         <div className="desc-item-pedido">
                             <p>Item:</p>
-                            <div className="desc-comida-pedido-item mb-3">
-                                <p><b>{descricaoPedidos.item?.quantidade}x   {descricaoPedidos.item?.comida?.nome}</b></p>
-                                <p><b>R$ {descricaoPedidos.item?.comida.preco}</b></p>
+                            {descricaoPedidos.items.map(item => (
+                                <div className="desc-comida-pedido-item mb-3" >
+                                    <p><b>{item.quantidade}x {item.comida?.nome}</b></p>
+                                    <p><b>R$ {item.comida?.preco}</b></p>
 
-
-                            </div>
+                                </div>
+                            ))}
 
                             <div className="vertical-row-desc"></div>
                             <div className="desc-comida-pedido">
                                 <p>Preço:</p>
-                                <p>R$ {(descricaoPedidos.item?.quantidade * descricaoPedidos.item?.comida?.preco).toFixed(2)}</p>
+                                <p>R$ {calcularValorTotalItens().toFixed(2)}</p>
                             </div>
                             <div className="desc-comida-pedido">
                                 <p>Frete:</p>
@@ -67,7 +93,7 @@ export function DescricaoPedidoCliente() {
                             </div>
                             <div className="desc-comida-pedido">
                                 <p>SubTotal:</p>
-                                <p>R$ {(frete + (descricaoPedidos.item?.quantidade * descricaoPedidos.item?.comida?.preco)).toFixed(2)}</p>
+                                <p>R$ {(frete + calcularValorTotalItens()).toFixed(2)}</p>
                             </div>
                             <div className="desc-comida-pedido">
                                 <p>Cupom:</p>
@@ -75,7 +101,7 @@ export function DescricaoPedidoCliente() {
                             </div>
                             <div className="desc-comida-pedido-total">
                                 <p><b>Total:</b></p>
-                                <p><b>R$ {((frete + (descricaoPedidos.item?.quantidade * descricaoPedidos.item?.comida?.preco)) - cupom).toFixed(2)}</b></p>
+                                <p><b>R$ {((frete + calcularValorTotalItens()) - cupom).toFixed(2)}</b></p>
                             </div>
                         </div>
                         <div className="vertical-row-desc"></div>
@@ -97,8 +123,31 @@ export function DescricaoPedidoCliente() {
                         </div>
                         <div className="vertical-row-desc"></div>
                         <div className="desc-infos-pedido">
-                            <h4>Status: </h4>
-                            <p>   {descricaoPedidos.status}</p>
+                            {descricaoPedidos.status === "Pendente" ? (
+                                <p>
+                                    <b>Status:</b> {descricaoPedidos.status}
+                                </p>
+                            ) : descricaoPedidos.status === "Aguardando confirmação" ? (
+                                <p>
+                                    Status: <b className="aguardando">{descricaoPedidos.status}</b>
+                                </p>
+                            ) : descricaoPedidos.status === "Confirmado" ? (
+                                <p>
+                                    Status:<b className="confirmado"> {descricaoPedidos.status}</b>
+                                </p>
+                            ) : descricaoPedidos.status === "A caminho" ? (
+                                <p>
+                                    Status: <b className="aCaminho">{descricaoPedidos.status}</b>
+                                </p>
+                            ) : descricaoPedidos.status === "Entregue" ? (
+                                <p>
+                                    Status: <b className="entregue">{descricaoPedidos.status}</b>
+                                </p>
+                            ) : descricaoPedidos.status === "Cancelado" ? (
+                                <p>
+                                    Status: <b className="cancelado">{descricaoPedidos.status}</b>
+                                </p>
+                            ) : null}
                         </div>
                         <div className="vertical-row-desc"></div>
 
