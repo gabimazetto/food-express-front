bbeaee130c53526bbc29e75877ecf55e311403d5
juliagascho <juliagascho@hotmail.com>
2023-05-23 14:56:43 -0300

    Refatoração página de editar cliente

--- src/components/ButtonNavigation/ButtonNavigation.jsx
+++ src/components/ButtonNavigation/ButtonNavigation.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom'; 
-import "./ButtonNavigation"
+import { useNavigate } from 'react-router-dom';
+import './ButtonNavigation.css';
 
-export  function ButtonNavigation({ text, route, className, type }) {
+export function ButtonNavigation({ text, route, className, type, icon }) {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -12,9 +12,8 @@ export  function ButtonNavigation({ text, route, className, type }) {
 
   return (
     <Button variant="primary" onClick={handleClick} className={className} type={type}>
+      {icon && <i className={icon}></i>}
       {text}
     </Button>
   );
 }
-
-
