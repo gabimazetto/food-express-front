fe9283b488707560b5e4f719416f5ddd126f08c1
vanessaneves-dev <vanessaneves.dev@gmail.com>
2023-05-24 01:28:30 -0300

    :recycle: refactor Refatoração da pagina de adicionar ou atualizar comida do  Restaurante

--- src/components/CustomInputPreco/CustomInputPreco.jsx
+++ src/components/CustomInputPreco/CustomInputPreco.jsx
@@ -12,7 +12,7 @@ export function CustomInputPreco({ type, placeholder, icon, value, onChange, cla
         <Form.Group className="mb-3 input-preco">
             <Form.Label>{label}</Form.Label>
             <InputGroup className="custon-input-group mt-2">
-                <InputGroup.Text className="">
+                <InputGroup.Text className="white">
                     R$
                 </InputGroup.Text>
                 <Form.Control className={className} type={type} placeholder={placeholder} value={value} onChange={onChange} />
