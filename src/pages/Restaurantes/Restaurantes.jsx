f227050497e67567be41113cd83c828c410be3b2
Diego Arruda <arrudadiego1@gmail.com>
2023-05-26 21:29:06 -0300

    Adição de um botão para informações do restaurante

--- src/pages/Restaurantes/Restaurantes.jsx
+++ src/pages/Restaurantes/Restaurantes.jsx
@@ -1,16 +1,15 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col, Modal } from "react-bootstrap";
 import { Loader } from "../../components/Loader/Loader";
 import { Link } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import "./Restaurante.css";
 import { ContextClient } from "../../contexts/ClientContext";
 import { ContextLogin } from "../../contexts/LoginContext";
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Offcanvas from "react-bootstrap/Offcanvas";
 import ReactStars from "react-stars";
 import { ButtonNavigation } from "../../components/ButtonNavigation/ButtonNavigation";
-
 
 export function Restaurantes() {
   const [restaurantes, setRestaurantes] = useState(null);
@@ -20,16 +19,25 @@ export function Restaurantes() {
   const [mediasAvaliacoes, setMediasAvaliacoes] = useState({});
   const [comentariosAvaliacoes, setComentariosAvaliacoes] = useState([]);
   const [show, setShow] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = (restauranteId) => {
+    setRestauranteId(restauranteId);
+    setShowModal(true);
+  };
+  const [restaurante, setRestaurante] = useState([]);
+  const [buscaInfo, setBuscaInfo] = useState(true);
+  const [restauranteId, setRestauranteId] = useState("");
 
   useEffect(() => {
     initializeTable();
   }, [idCli]);
 
-
   function initializeTable() {
-    axios.get("http://localhost:3001/restaurantes", config)
+    axios
+      .get("http://localhost:3001/restaurantes", config)
       .then((response) => {
         const restaurantes = response.data.map((restaurante) => {
           const {
@@ -56,7 +64,8 @@ export function Restaurantes() {
       restauranteId,
       clienteId: idCli,
     };
-    axios.post("http://localhost:3001/restaurantes/favoritos", data, config)
+    axios
+      .post("http://localhost:3001/restaurantes/favoritos", data, config)
       .then((response) => {
         toast.success("Adicionado aos Favoritos", {
           position: "bottom-right",
@@ -82,7 +91,10 @@ export function Restaurantes() {
     try {
       const medias = {};
       for (const restaurante of restaurantes) {
-        const response = await axios.get(`http://localhost:3001/avaliacaos/media/${restaurante.id}`, config);
+        const response = await axios.get(
+          `http://localhost:3001/avaliacaos/media/${restaurante.id}`,
+          config
+        );
         const media = response.data.media;
         medias[restaurante.id] = media;
       }
@@ -94,7 +106,10 @@ export function Restaurantes() {
 
   async function buscarComentariosAvaliacoes(restauranteId) {
     try {
-      const response = await axios.get(`http://localhost:3001/avaliacaos/${restauranteId}`, config);
+      const response = await axios.get(
+        `http://localhost:3001/avaliacaos/${restauranteId}`,
+        config
+      );
       const comentarios = response.data.map((avaliacao) => ({
         comentario: avaliacao.comentario,
         nota: avaliacao.avaliacao,
@@ -106,17 +121,40 @@ export function Restaurantes() {
     }
   }
 
+  useEffect(() => {
+    const detalhe = async () => {
+      try {
+        if (restauranteId) {
+          const detalhes = await axios.get(
+            `http://localhost:3001/restaurantes/${restauranteId}`,
+            config
+          );
+          console.log(detalhes.data);
+          setRestaurante(detalhes.data);
+          setBuscaInfo(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    detalhe();
+  }, [restauranteId]);
+
   return (
     <Container>
       <div className="navbar nav-rest">
         <h1 className="mt-4 mb-4">Restaurantes</h1>
 
         <form className="form-rest form-inline">
-          <input type="text" className="search-rest"
+          <input
+            type="text"
+            className="search-rest"
             value={busca}
-            onChange={(e) => setBusca(e.target.value)} placeholder="Pesquise por nome ou cidade" />
+            onChange={(e) => setBusca(e.target.value)}
+            placeholder="Pesquise por nome ou cidade"
+          />
         </form>
-
       </div>
 
       {restaurantes === null ? (
@@ -129,8 +167,7 @@ export function Restaurantes() {
               const cidade = restaurante.endereco.cidade.toLowerCase();
               const termoBusca = busca.toLowerCase();
               return (
-                nomeFantasia.includes(termoBusca) ||
-                cidade.includes(termoBusca)
+                nomeFantasia.includes(termoBusca) || cidade.includes(termoBusca)
               );
             })
             .map((restaurante) => {
@@ -138,19 +175,20 @@ export function Restaurantes() {
                 <Col md={4} key={restaurante.id}>
                   <Card className="mb-4 py-4 card-principal">
                     <Card.Body className="card-rest">
-                      <Card.Title className="fs-3 title-rest">{restaurante.nomeFantasia}</Card.Title>
+                      <Card.Title className="fs-3 title-rest">
+                        {restaurante.nomeFantasia}
+                      </Card.Title>
 
-                      <Card.Subtitle className="mb-2 text-muted">
-                        Cidade: {restaurante.endereco.cidade}
-                      </Card.Subtitle>
                       <div className="icons-rest">
-                        <Button className="detalhes my-button-not-filled-rest"
+                        <Button
+                          className="detalhes my-button-not-filled-rest "
                           as={Link}
                           to={`/cliente/restaurante/cardapio/${restaurante.id}`}
                         >
-                          <i className="bi bi-book"></i> Mais Detalhes
+                          <i className="bi bi-book"></i> Cardápio
                         </Button>
-                        <Button className="my-button-not-filled-rest"
+                        <Button
+                          className="my-button-not-filled-rest"
                           type="submit"
                           onClick={() => FavRestaurante(restaurante.id)}
                         >
@@ -159,25 +197,39 @@ export function Restaurantes() {
                           ) : (
                             <i className="bi bi-heart"></i>
                           )}
-
                         </Button>
-
-                        <Button className="my-button-not-filled-rest" onClick={() => {
-                          handleShow();
-                          buscarComentariosAvaliacoes(restaurante.id);
-                        }}>
+                        <Button
+                          onClick={() => {
+                            handleShowModal(restaurante.id);
+                          }}
+                          className="my-button-not-filled-rest"
+                        >
+                          <i className="bi bi-info-circle-fill"></i>
+                        </Button>
+                        <Button
+                          className="my-button-not-filled-rest"
+                          onClick={() => {
+                            handleShow();
+                            buscarComentariosAvaliacoes(restaurante.id);
+                          }}
+                        >
                           <ReactStars
                             count={5}
                             value={mediasAvaliacoes[restaurante.id]}
                             size={24}
-                            color2={'#f06000'}
-                            edit={false}  // Isso desabilita a possibilidade do usuário alterar o valor
+                            color2={"#f06000"}
+                            edit={false} // Isso desabilita a possibilidade do usuário alterar o valor
                           />
                         </Button>
 
                         <Offcanvas show={show} onHide={handleClose}>
-                          <Offcanvas.Header closeButton className="offcanvas-header">
-                            <Offcanvas.Title className="offcanvas-title">Avaliações</Offcanvas.Title>
+                          <Offcanvas.Header
+                            closeButton
+                            className="offcanvas-header"
+                          >
+                            <Offcanvas.Title className="offcanvas-title">
+                              Avaliações
+                            </Offcanvas.Title>
                           </Offcanvas.Header>
                           <Offcanvas.Body className="offcanvas-body">
                             {comentariosAvaliacoes.map((comentario, index) => (
@@ -190,11 +242,9 @@ export function Restaurantes() {
                             ))}
                           </Offcanvas.Body>
                         </Offcanvas>
-
                       </div>
                     </Card.Body>
                   </Card>
-
                 </Col>
               );
             })}
@@ -209,104 +259,36 @@ export function Restaurantes() {
           tooltipContent="Voltar para a home"
         />
       </div>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>Informações</Modal.Header>
+        <Modal.Body>
+          <div className="container">
+            {buscaInfo ? (
+              <></>
+            ) : (
+              <Card className="card-restaurante">
+                <Card.Body>
+                  <Card.Title>{restaurante.nomeFantasia}</Card.Title>
+                  <b>CNPJ: </b>
+                  {restaurante.cnpj} <br />
+                  <b>Email: </b>
+                  {restaurante.email} <br />
+                  <b>Rua: </b>
+                  {restaurante.endereco.rua} <br />
+                  <b>Número: </b>
+                  {restaurante.endereco.numero} <br />
+                  <b>Cidade: </b>
+                  {restaurante.endereco.cidade} <br />
+                  <b>Estado: </b>
+                  {restaurante.endereco.uf} <br />
+                  <b>Telefone: </b>
+                  {restaurante.telefone} <br />
+                </Card.Body>
+              </Card>
+            )}
+          </div>
+        </Modal.Body>
+      </Modal>
     </Container>
-
-
-    // <div className="container">
-    //   <div className="navbar navbar-light bg-light">
-    //     <h1>Restaurantes</h1>
-    //     <div className="search-wrapper">
-    //       <i className="bi bi-search"></i>
-    //       <input
-    //         type="text"
-    //         value={busca}
-    //         onChange={(e) => setBusca(e.target.value)}
-    //         placeholder="Nome/Cidade/Rua"
-    //       />
-    //     </div>
-    //   </div>
-    //   {restaurantes === null ? (
-    //     <Loader />
-    //   ) : (
-    //     <Table striped bordered hover>
-    //       <thead>
-    //         <tr>
-    //           <th>Nome Fantasia</th>
-    //           <th>Cidade</th>
-    //           <th>Rua</th>
-    //           <th>Mais Detalhes</th>
-    //           <th>Adicionar aos Favoritos</th>
-    //           <th>Avaliações</th>
-    //         </tr>
-    //       </thead>
-    //       <tbody>
-    //         {restaurantes
-    //           .filter((restaurante) => {
-    //             const nomeFantasia = restaurante.nomeFantasia.toLowerCase();
-    //             const cidade = restaurante.endereco.cidade.toLowerCase();
-    //             const rua = restaurante.endereco.rua.toLowerCase();
-    //             const termoBusca = busca.toLowerCase();
-    //             return (
-    //               nomeFantasia.includes(termoBusca) ||
-    //               cidade.includes(termoBusca) ||
-    //               rua.includes(termoBusca)
-    //             );
-    //           })
-    //           .map((restaurante) => {
-    //             return (
-    //               <tr key={restaurante.id}>
-    //                 <td>{restaurante.nomeFantasia}</td>
-    //                 <td>{restaurante.endereco.cidade}</td>
-    //                 <td>{restaurante.endereco.rua}</td>
-    //                 <td>
-    //                   <Button
-    //                     as={Link}
-    //                     to={`/cliente/restaurante/cardapio/${restaurante.id}`}
-    //                   >
-    //                     <i className="bi bi-book"></i>
-    //                   </Button>
-    //                 </td>
-    //                 <td>
-    //                   <Button
-    //                     type="submit"
-    //                     onClick={() => FavRestaurante(restaurante.id)}
-    //                   >
-    //                     {restaurante.favorito ? (
-    //                       <i className="bi bi-heart-fill"></i>
-    //                     ) : (
-    //                       <i className="bi bi-heart"></i>
-    //                     )}
-    //                   </Button>
-    //                 </td>
-    //                 {/* Inicio OffCanvas */}
-    //                 <Button onClick={() => {
-    //                   handleShow();
-    //                   buscarComentariosAvaliacoes(restaurante.id);
-    //                 }}>
-    //                   <td>{mediasAvaliacoes[restaurante.id]}</td>
-    //                 </Button>
-    //                 <Offcanvas show={show} onHide={handleClose}>
-    //                   <Offcanvas.Header closeButton>
-    //                     <Offcanvas.Title>Avaliações</Offcanvas.Title>
-    //                   </Offcanvas.Header>
-    //                   <Offcanvas.Body>
-    //                     {comentariosAvaliacoes.map((comentario, index) => (
-    //                       <div key={index}>
-    //                         <p>Cliente: {comentario.nomeCliente}</p>
-    //                         <p>Comentário: {comentario.comentario}</p>
-    //                         <p>Nota: {comentario.nota}</p>
-    //                       </div>
-    //                     ))}
-    //                   </Offcanvas.Body>
-    //                 </Offcanvas>
-    //                 {/* Final OffCanvas */}
-    //               </tr>
-    //             );
-    //           })}
-    //       </tbody>
-    //     </Table>
-    //   )
-    //   }
-    // </div >
   );
 }
